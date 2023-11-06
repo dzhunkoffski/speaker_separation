626486408129a114b04f7dc1c423d8b7e0b138e8
Kirill <dzhunkofffski@gmail.com>
2023-11-07 02:25:37 +0300

    o

--- ss/augmentations/wave_augmentations/__init__.py
+++ ss/augmentations/wave_augmentations/__init__.py
@@ -1,5 +1,11 @@
 from ss.augmentations.wave_augmentations.Gain import Gain
+from ss.augmentations.wave_augmentations.GaussianNoise import GaussianNoise
+from ss.augmentations.wave_augmentations.PitchShift import PitchShift
+from ss.augmentations.wave_augmentations.SpeedPerturbation import SpeedPerturbation
 
 __all__ = [
-    "Gain"
+    "Gain",
+    "GaussianNoise",
+    "PitchShift",
+    "SpeedPerturbation"
 ]
